3a024e1c3b6aae6590af9ffefe94b80ae553fd3c
Jorge <jorgediazok@gmail.com>
2020-05-14 20:38:10 -0300

    project fail. Abort mission

--- index.js
+++ index.js
@@ -102,8 +102,34 @@ function totalCost(product) {
 }
 
 function displayCart() {
-  let carItems = localStorage.getItem('productsInCart');
-  carItems = JSON.parse(carItems);
+  let cartItems = localStorage.getItem('productsInCart');
+  cartItems = JSON.parse(cartItems);
+  let productContainer = document.querySelector('.products');
+  let cartCost = localStorage.getItem('totalCost');
+  if (cartItems && productContainer) {
+    productContainer.innerHTML = '';
+    Object.values(cartItems).map((item) => {
+      productContainer.innerHTML += `
+      <div class="product">
+        <ion-icon name="close-circle"></ion-icon>
+        <img src="./images/${item.tag}.jpg">
+        <span>${item.name}</span>
+      </div>
+      <div class="price">$${item.price},00</div>
+      <div class="quantity">
+        <ion-icon class="decrease" name="arrow-dropleft-circle"></ion-icon>
+        <span>${item.inCart}</span>
+        <ion-icon class="increase" name="arrow-dropright-circle"></ion-icon>
+      </div>
+      <div class="total">$${item.inCart * item.price},00</div>
+      `;
+    });
+    productContainer.innerHTML += `
+    <div class="basketTotalContainer">
+    <h4 class="basketTotalTitle">Basket Total</h4>
+    <h4 class="basketTotal">$${cartCost},00</h4>
+    </div>`;
+  }
 }
 
 onLoadCartNumbers();
